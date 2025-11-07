32f45a5b6d4a0bc9dc92299603d78c9c77cc8e39
Raphael Squinzari Dias Barbosa <10737008@educa.mackenzie.br>
2025-11-07 20:20:03 -0300

    feat: projeto iniciado

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        public class Disciplina {
+            private String nome;
+            private String codigo;
+
+            public Disciplina(String nome, String codigo) {
+                this.nome = nome;
+                this.codigo = codigo;
+            }
+
+            public String Getnome()
+                return nome;
+            }
+        }
+    }
+}
